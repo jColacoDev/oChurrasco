a44f9695b55ffe4aa8ce1b50b9a16de828962f07
joCol-git <joao.r.colaco@gmail.com>
2023-03-03 18:23:44 +0000

    dropbox start

--- client/src/jcPACK/Apps/app/routes/Home.jsx
+++ client/src/jcPACK/Apps/app/routes/Home.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Gallery from '../../../components/Gallery/Gallery'
 import { productsData } from '../Products'
 import ContactForm from '../../../components/ContactForm/ContactForm'
 import GalleryCaroussel from '../../../components/GalleryCaroussel/GalleryCaroussel'
+import {Dropbox} from 'dropbox';
 
 export default function Home() {
+
+  let dbx = new Dropbox({ accessToken:  import.meta.env.VITE_DROPBOX_TOKEN });
+
+  useEffect(()=>{
+    dbx.usersGetCurrentAccount()
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+
+    dbx.filesListFolder({path: ''})
+    .then(function(response) {
+      console.log(response.entries);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+  },[])
+
   return (
     <div className='Home'>
         <span className='heading'>
