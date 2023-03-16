229ddd4121243c1d6a39ef40d909537c1cc96cc3
joCol-git <joao.r.colaco@gmail.com>
2023-03-16 17:44:39 +0000

    no ignore

---
+++ client/src/jcPACK/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoBoMYj6QyPUeIFbUqINPpItH7Kl-tYgc",
+  authDomain: "ergoface-379714.firebaseapp.com",
+  projectId: "ergoface-379714",
+  storageBucket: "ergoface-379714.appspot.com",
+  messagingSenderId: "525831025383",
+  appId: "1:525831025383:web:e0d27047be45852616e842",
+  measurementId: "G-RPSVDB14K4"
+};
